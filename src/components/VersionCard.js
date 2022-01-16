fc00483a5421456990350c37c34443985216fddc
Massimiliano <massimiliano.albini@gmail.com>
2022-01-16 17:50:54 +0100

    last adjustments

--- src/components/VersionCard.js
+++ src/components/VersionCard.js
@@ -2,14 +2,27 @@ import React from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faSquare } from "@fortawesome/free-regular-svg-icons"
 
-export default function VersionCard(props) {
+export default function VersionCard({version, step1, step2, step3, step4}) {
     return (
         <div className="versionCard">
-            <h1 className="versionCard--title">Version {props.version}</h1>
-            <p className="versionCard--feature"><FontAwesomeIcon icon={faSquare}/> Feature 1</p>
-            <p className="versionCard--feature"><FontAwesomeIcon icon={faSquare}/> Feature 1</p>
-            <p className="versionCard--feature"><FontAwesomeIcon icon={faSquare}/> Feature 1</p>
-            <p className="versionCard--feature"><FontAwesomeIcon icon={faSquare}/> Feature 1</p>
+            <h1 className="versionCard--title">Face {version}</h1>
+            <div className="versionCard--flex">
+                <p className="versionCard--square"><FontAwesomeIcon icon={faSquare}/></p>
+                <p className="versionCard--feature">{step1}</p>
+            </div>
+            <div className="versionCard--flex">
+                <p className="versionCard--square"><FontAwesomeIcon icon={faSquare}/></p>
+                <p className="versionCard--feature">{step2}</p>
+            </div>
+            <div className="versionCard--flex">
+                <p className="versionCard--square"><FontAwesomeIcon icon={faSquare}/></p>
+                <p className="versionCard--feature">{step3}</p>
+            </div>
+            {step4 &&
+            <div className="versionCard--flex">
+                <p className="versionCard--square"><FontAwesomeIcon icon={faSquare}/></p>
+                <p className="versionCard--feature">{step4}</p>
+            </div>}
         </div>
     )
 }
